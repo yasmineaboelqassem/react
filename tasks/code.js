3fec6582d4e1a03afbcb7ad2442625f9e030e211
yasmineaboelqassem <159438555+yasmineaboelqassem@users.noreply.github.com>
2024-02-19 19:44:11 +0200

    Add files via upload

---
+++ tasks/code.js
@@ -0,0 +1,80 @@
+// class foursideshape{
+//     constructor(slide1,slide2,slide3,slide4)
+// }
+//     this.slide1=slide1
+//     this.slide2=slide2
+//     this.slide3=slide3
+//     this.slide4=slide4
+// {
+// this.slide1=slide1
+// this.slide2=slide2
+// this.slide3=slide3
+// this.slide4=slide4
+// }
+    
+// class triangle extends foursideshape {
+//     constructor(slide1,slide2,slide3)
+//     super(slide1,slide2,slide3,)
+
+// }
+// printtrinagle(slide1,slide2,slide3)
+// {
+//     return  slide1,slide2,slide3 ;
+// }
+// area(slide1,slide2)
+//  { 
+// let shape = (0.5*this.slide2)*this.slide1
+// console.log(shape) ;
+// };
+
+// sum= new sum (15,10,8);
+// console.log(sum) ;
+// console.log(sum.permiter()) ;
+// console.log(sum.area()) ;
+
+
+
+
+// task num \\
+// class shape {
+//     constructor(slide){
+//         this.slide=slide ;
+//     }
+//     space () {
+//         return this.slide * this.slide ;
+//     }
+//     ocean () {
+//         return 4 * this.slide ;
+//     }
+// }
+// class rectangle extends shape {
+//     function question () {
+//         let width = parseFloat("width");
+//         let height = parseFloat ("height");
+//         let rectangle1 = new rectangle (width);
+//         let rectangle2= new rectangle (height);
+//         return question ;
+//     }
+//     console.log ("space:",rectangle.space)
+//     console.log ("ocean:",rectangle.ocean)
+
+
+
+
+// task num 2
+
+class shape {
+    constructor ( width , height)
+    {
+        this.width=width
+        this.height=height
+    }
+}
+class shapeing extends shape {
+    constructor(sidelength) {
+        super(sidelength);
+    }
+}
+let shapeing=new shapeing ()
+console.log (shapeing.width);
+console.log (shapeing.height);
